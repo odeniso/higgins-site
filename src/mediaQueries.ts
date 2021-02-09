8fd7041b00558d7c1341ae129b064939161d5547
odeniso <sined.drahcir@gmail.com>
2021-02-09 13:35:36 +0100

    make values on index page big screen specific

---
+++ src/mediaQueries.ts
@@ -0,0 +1,8 @@
+const getMediaQ = (minPixelWidth: number) =>
+  `@media (min-width: ${minPixelWidth}px)`;
+
+export const mediaQs = {
+  isMinSmall: getMediaQ(360),
+  isMinLarge: getMediaQ(1024),
+  isMinHuge: getMediaQ(1800),
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,20 @@
 import * as React from "react";
 
 import { css } from "@emotion/react";
-
 import { mapValues } from "lodash";
 
 import Navbar from "../components/Navbar";
-
 import Layout from "../components/Layout";
 import Landing from "../components/Landing";
 import AboutUs from "../components/AboutUs";
-
-import "../fonts/fonts.css";
 import ProductInfo from "../components/ProductInfo";
 import { ContactUs } from "../components/ContactUs";
+import DogBadge from "../illustrations/DogBadge";
+
+import { mediaQs } from "../mediaQueries";
+
+import "../fonts/fonts.css";
+import { colors } from "../params";
 
 const partSpacingVert = "96px";
 
@@ -22,50 +24,70 @@ const anchorNameMap = {
   contact: "contact",
 };
 
+const NavBarContainer = () => (
+  <div>
+    <div
+      css={css`
+        ${mediaQs.isMinLarge} {
+          display: none;
+        }
+      `}
+    >
+      <DogBadge
+        css={css`
+          height: 80px;
+          margin: 24px;
+        `}
+        color={colors.stromboli}
+      />
+    </div>
+    <div
+      css={css`
+        display: none;
+        ${mediaQs.isMinLarge} {
+          display: block;
+        }
+      `}
+    >
+      <Navbar
+        css={css`
+          margin: 32px 0;
+        `}
+        urlMap={mapValues(anchorNameMap, (anchorName) => `#${anchorName}`)}
+      />
+    </div>
+  </div>
+);
+
+const ContentRow: React.FunctionComponent = ({ children }) => (
+  <div
+    css={css`
+      ${mediaQs.isMinLarge} {
+        margin: ${partSpacingVert} 0;
+      }
+    `}
+  >
+    {children}
+  </div>
+);
+
 const IndexPage = () => {
   return (
     <Layout>
-      <div
-        css={css`
-          display: flex;
-          justify-content: center;
-        `}
-      >
-        <div
-          css={css`
-            justify-content: center;
-          `}
-        >
-          <Navbar
-            css={css`
-              margin: 32px 0;
-            `}
-            urlMap={mapValues(anchorNameMap, (anchorName) => `#${anchorName}`)}
-          />
-          <Landing
-            css={css`
-              margin: ${partSpacingVert} 0;
-            `}
-          />
-          <AboutUs
-            css={css`
-              margin: ${partSpacingVert} 0;
-            `}
-            id={anchorNameMap.about}
-          />
-          <ProductInfo
-            css={css`
-              margin: ${partSpacingVert} 0;
-            `}
-            id={anchorNameMap.product}
-          />
-          <ContactUs
-            css={css`
-              margin: ${partSpacingVert} 0;
-            `}
-            id={anchorNameMap.contact}
-          />
-        </div>
+      <div>
+        <NavBarContainer />
+        <ContentRow>
+          <Landing />
+        </ContentRow>
+        <ContentRow>
+          <AboutUs id={anchorNameMap.about} />
+        </ContentRow>
+        <ContentRow>
+          <ProductInfo id={anchorNameMap.product} />
+        </ContentRow>
+        <ContentRow>
+          <ContactUs id={anchorNameMap.contact} />
+        </ContentRow>
       </div>
     </Layout>
   );
