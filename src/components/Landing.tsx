0d5db8acd37de85f379c5f5e2a1be3cb9cd028db
odeniso <sined.drahcir@gmail.com>
2021-02-12 17:14:34 +0100

    change landing style a little bit

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -18,13 +18,15 @@ const Landing = ({
   <div
     css={css`
       display: flex;
-      flex-direction: column;
+      flex-direction: column-reverse;
       margin-left: ${margins.side};
       margin-right: ${margins.side};
+      padding-bottom: 80px;
       ${mediaQs.isMinLarge} {
         margin-left: 0;
         margin-right: 0;
         flex-direction: row;
+        padding-bottom: 0;
       }
     `}
     className={className}
@@ -32,12 +34,15 @@ const Landing = ({
   >
     <div
       css={css`
+        align-items: center;
         display: flex;
         justify-content: center;
         margin-bottom: 24px;
         font-family: "Cheltenham";
+        flex-direction: column;
         ${mediaQs.isMinLarge} {
-          padding-top: 5%;
+          padding-top: 8%;
+          flex-direction: row;
           margin-bottom: 0;
           margin-right: 24px;
           display: block;
@@ -50,48 +55,55 @@ const Landing = ({
         }
       `}
     >
-      <h1
-        css={css`
-          font-size: 42px;
-          text-align: center;
-          ${mediaQs.isMinLarge} {
-            font-size: 92px;
-            margin-bottom: 40px;
-            text-align: left;
-          }
-          ${mediaQs.isMinHuge} {
-            margin-bottom: 48px;
-            font-size: 140px;
-          }
-          margin: 0;
-        `}
-      >
-        So wie's sein soll
-      </h1>
       <div
         css={css`
-          display: none;
+          color: #000;
+          font-family: "Martha";
+          text-align: center;
+          font-size: 16px;
           ${mediaQs.isMinLarge} {
+            text-align: left;
             font-size: 24px;
-            display: block;
           }
           ${mediaQs.isMinHuge} {
             font-size: 36px;
           }
         `}
       >
-        Endlich wertvolles Hundefutter
+        ENDLICH WERTVOLLES HUNDEFUTTER
       </div>
+      <h1
+        css={css`
+          font-size: 42px;
+          text-align: center;
+          margin: 0;
+          margin-top: 24px;
+          ${mediaQs.isMinLarge} {
+            font-size: 92px;
+            margin-bottom: 40px;
+            margin-top: 0;
+            text-align: left;
+          }
+          ${mediaQs.isMinHuge} {
+            margin-bottom: 48px;
+            font-size: 140px;
+          }
+        `}
+      >
+        So wie es sein soll
+      </h1>
     </div>
     <div
       css={css`
         display: flex;
         justify-content: center;
         align-items: center;
+        margin-bottom: 24px;
         ${mediaQs.isMinLarge} {
           flex: 1;
           justify-content: flex-end;
           margin-right: ${margins.sideLarge};
+          margin-bottom: 0;
         }
         ${mediaQs.isMinHuge} {
           margin-right: ${marginHuge};
