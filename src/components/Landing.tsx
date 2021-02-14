4f44355e9acc5d80cc5eb3059ee5ed43915700a7
odeniso <sined.drahcir@gmail.com>
2021-02-15 00:16:09 +0100

    margins were affecting flex item sizing

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -19,11 +19,9 @@ const Landing = ({
     css={css`
       display: flex;
       flex-direction: column;
-      margin-left: ${margins.side};
       margin-right: 0;
       ${mediaQs.isMinLarge} {
         margin-left: 0;
-        margin-right: 0;
         flex-direction: row;
         padding-bottom: 0;
       }
@@ -33,67 +31,75 @@ const Landing = ({
   >
     <div
       css={css`
-        align-items: center;
         display: flex;
+        flex: 1;
         justify-content: center;
-        margin-bottom: 24px;
-        font-family: ${fonts.serif};
-        flex-direction: column;
-        ${mediaQs.isMinLarge} {
-          padding-top: 8%;
-          flex-direction: row;
-          margin-bottom: 0;
-          margin-right: 24px;
-          display: block;
-          margin-left: ${margins.sideLarge};
-          color: ${theme.headingColor};
-          flex: 1;
-        }
-        ${mediaQs.isMinHuge} {
-          margin-left: ${marginHuge};
-        }
       `}
     >
       <div
         css={css`
-          color: #000;
-          font-family: ${fonts.sansSerifTitle};
-          text-align: center;
-          max-width: 60%;
-          font-size: 16px;
+          align-items: center;
+          display: flex;
+          justify-content: center;
+          margin-bottom: 24px;
+          font-family: ${fonts.serif};
+          flex-direction: column;
           ${mediaQs.isMinLarge} {
-            color: ${theme.eyebrowTitleColor};
-            max-width: none;
-            text-align: left;
-            font-size: 24px;
+            padding-top: 8%;
+            flex-direction: row;
+            margin-bottom: 0;
+            margin-right: 24px;
+            display: block;
+            margin-left: ${margins.sideLarge};
+            color: ${theme.headingColor};
+            flex: 1;
           }
           ${mediaQs.isMinHuge} {
-            font-size: 36px;
+            margin-left: ${marginHuge};
           }
         `}
       >
-        ENDLICH WERTVOLLES HUNDEFUTTER
+        <div
+          css={css`
+            color: #000;
+            font-family: ${fonts.sansSerifTitle};
+            text-align: center;
+            max-width: 60%;
+            font-size: 16px;
+            ${mediaQs.isMinLarge} {
+              color: ${theme.eyebrowTitleColor};
+              max-width: none;
+              text-align: left;
+              font-size: 24px;
+            }
+            ${mediaQs.isMinHuge} {
+              font-size: 36px;
+            }
+          `}
+        >
+          ENDLICH WERTVOLLES HUNDEFUTTER
+        </div>
+        <h1
+          css={css`
+            font-size: 42px;
+            text-align: center;
+            margin: 0;
+            margin-top: 24px;
+            ${mediaQs.isMinLarge} {
+              font-size: 92px;
+              margin-bottom: 40px;
+              margin-top: 0;
+              text-align: left;
+            }
+            ${mediaQs.isMinHuge} {
+              margin-bottom: 48px;
+              font-size: 140px;
+            }
+          `}
+        >
+          So wie es sein soll
+        </h1>
       </div>
-      <h1
-        css={css`
-          font-size: 42px;
-          text-align: center;
-          margin: 0;
-          margin-top: 24px;
-          ${mediaQs.isMinLarge} {
-            font-size: 92px;
-            margin-bottom: 40px;
-            margin-top: 0;
-            text-align: left;
-          }
-          ${mediaQs.isMinHuge} {
-            margin-bottom: 48px;
-            font-size: 140px;
-          }
-        `}
-      >
-        So wie es sein soll
-      </h1>
     </div>
     <div
       css={css`
@@ -104,11 +110,7 @@ const Landing = ({
         ${mediaQs.isMinLarge} {
           flex: 1;
           justify-content: flex-end;
-          margin-right: ${margins.sideLarge};
           margin-bottom: 0;
-        }
-        ${mediaQs.isMinHuge} {
-          margin-right: ${marginHuge};
         }
       `}
     >
